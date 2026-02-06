d573c37d2833246bca234cc73e587c8cb9b57bc6
David Ginten <david.ginten@rwth-aachen.de>
2026-02-06 11:37:20 +0100

    No tokenizer needed to get buffers

--- src/bert-base-uncased/get_buffers.py
+++ src/bert-base-uncased/get_buffers.py
@@ -1,9 +1,6 @@
-from transformers import BertTokenizer, BertModel
+from transformers import BertModel
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained("bert-base-uncased")
-text = "This is a sample input for BERT model export."
-encoded_input = tokenizer(text, return_tensors='pt')
 
 params = dict(model.named_buffers(remove_duplicate=False))
 
